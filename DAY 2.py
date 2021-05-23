fbad8fe6346c1cb0fdc9ae00596fde90e6a10e38
rsaiteja903 <84631631+rsaiteja903@users.noreply.github.com>
2021-05-23 11:41:44 +0530

    Add files via upload

---
+++ DAY 2.py
@@ -0,0 +1,6 @@
+shop="Chaitanya Medical store"
+miles=10
+cost=1000.00
+
+print("The shop name is {}.\nIt is {} miles away ".format(shop, miles))
+print("I bought medicines of total cost {}/- from the shop {}".format(cost,shop))
